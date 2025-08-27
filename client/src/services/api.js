872ac5f1ca9fc685a9093c9d69b1b91b05de08b1
roger <rogerbap@outlook.com>
2025-08-26 20:53:36 -0500

    final commit for backend and frontend deployment with url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://codequest-backend-funx.onrender.com/api'
+  : 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
